db041248d8840f445070609f208a3b766e2e7b96
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-30 10:16:40 -0400

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ const Product = require('../models/Product')
 
 // Create operation
 const createProduct = async (req, res, next) => {
-  console.log(req.user)
+  const createdBy = req.user.userId
   try {
     const {
       name,
@@ -27,6 +27,7 @@ const createProduct = async (req, res, next) => {
       storagePoint,
       category,
       isAvailable,
+      createdBy,
     })
     res.status(StatusCodes.CREATED).json({
       success: true,
@@ -40,12 +41,44 @@ const createProduct = async (req, res, next) => {
 
 // Read operation: Get all products
 const getAllProducts = async (req, res, next) => {
+  const searchQuery = req.query.search // Get the search query from the request query parameters
+  const page = parseInt(req.query.page) || 1 // Get the page number from the request query parameters, default is 1
+  const limit = parseInt(req.query.limit) || 10 // Get the limit from the request query parameters, default is 10
+  const sort = req.query.sort || '-createdAt' // Get the sort parameter from the request query parameters, default is -createdAt
+
+  // Create a regex pattern to match the search query case-insensitively
+  const regexPattern = new RegExp(searchQuery, 'i')
+
+  const query = {
+    $or: [
+      { name: regexPattern },
+      { description: regexPattern },
+      { category: regexPattern },
+    ],
+  }
   try {
-    const products = await Product.find()
-    const count = await Product.countDocuments()
-    res
-      .status(200)
-      .json({ success: true, message: 'All products', count, result: products })
+    const totalSearchCount = await Product.countDocuments(query) // Get the total count of matching users
+    const totalData = await Product.countDocuments()
+
+    const totalPages = Math.ceil(totalSearchCount / limit) // Calculate the total number of pages based on the limit
+
+    const offset = (page - 1) * limit // Calculate the offset based on the page and limit
+
+    const product = await Product.find(query, '-__v -createdBy -updatedBy')
+      .sort(sort) // Sort the users based on the provided sort parameter
+      .skip(offset)
+      .limit(limit)
+
+    res.status(200).json({
+      success: true,
+      message: 'All Products',
+      totalData,
+      totalSearchCount,
+      countOnPage: product.length,
+      totalPages,
+      currentPage: page,
+      result: product,
+    })
   } catch (err) {
     next(err)
   }
