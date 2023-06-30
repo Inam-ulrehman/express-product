75455488a9b37b2254b9acef5d5a0ee30d3886cb
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-30 13:45:56 -0400

    update

--- models/Product.js
+++ models/Product.js
@@ -1,59 +1,62 @@
 const mongoose = require('mongoose')
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    maxlength: [50, 'Name cannot be more than 50 characters'],
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name cannot be more than 50 characters'],
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: [500, 'Description cannot be more than 500 characters'],
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, 'Price must be a positive number'],
+    },
+    purchasePrice: {
+      type: Number,
+      required: true,
+      min: [0, 'Purchase price must be a positive number'],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, 'Quantity must be a positive number'],
+    },
+    storagePoint: {
+      type: String,
+      trim: true,
+      maxlength: [50, 'Storage point cannot be more than 50 characters'],
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [50, 'Category cannot be more than 50 characters'],
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    // Add more fields as needed
   },
-  description: {
-    type: String,
-    required: true,
-    maxlength: [500, 'Description cannot be more than 500 characters'],
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, 'Price must be a positive number'],
-  },
-  purchasePrice: {
-    type: Number,
-    required: true,
-    min: [0, 'Purchase price must be a positive number'],
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, 'Quantity must be a positive number'],
-  },
-  storagePoint: {
-    type: String,
-    trim: true,
-    maxlength: [50, 'Storage point cannot be more than 50 characters'],
-  },
-  category: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: [50, 'Category cannot be more than 50 characters'],
-  },
-  isAvailable: {
-    type: Boolean,
-    default: true,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  updatedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  // Add more fields as needed
-})
+  { timestamps: true }
+)
 
 const Product = mongoose.model('Product', productSchema)
 
